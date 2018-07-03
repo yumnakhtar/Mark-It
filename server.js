7930802724f6cf3424c48bb693fd6617a3a1aa8e
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-07-03 16:44:30 -0700

    install sequelize

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(routes);
 
 // Start the API server
-app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  db.sequelize.sync({force:true}).then(function() {
+    app.listen(PORT, function() {
+      console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+    });
   });
