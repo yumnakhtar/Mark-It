bc68b2a989bd6b9b8aae20ba612db2a50f99ec68
lesleyfon <lesleybanadzem2017@gmail.com>
2018-07-03 21:19:48 -0700

    Created the login form

--- client/src/components/LoginForm/InputPassword.js
+++ client/src/components/LoginForm/InputPassword.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const InputPassword = props => (
     <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id=""  placeholder="Enter email" {...props}/>
+    <label for="password1">Password</label>
+    <input type="password" class="form-control" id=""  placeholder="Password" {...props}/>
     
   </div>
 )
--- client/src/components/LoginForm/LoginForm.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const LoginForm = () => (
-   <div>
-       <form> </form>
-   </div>
-)
