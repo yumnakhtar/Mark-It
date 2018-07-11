17488ae562c684cc3757f4a7d8858fc9657bc453
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-07-11 16:27:26 -0700

    clean up code

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -9,17 +9,15 @@ class Sidebar extends Component {
 		this.handleFocus = this.handleFocus.bind(this);
 		this.handleChange = this.handleChange.bind(this);
         this.handleKeypress = this.handleKeypress.bind(this);
-        this.handleBlur = this.handleBlur.bind(this);
-		this.handleClick = this.handleClick.bind(this);
-		
+        this.handleBlur = this.handleBlur.bind(this);		
         this.state = {
             userId: "6e91a560-80a8-11e8-a8aa-71792012fa45",
             content_add: "",
 			width: 100,
             categories: []
         };
-        // this.lastId = -1;
     }
+
     componentDidMount() {
         this.loadCategories();
     };
@@ -27,7 +25,6 @@ class Sidebar extends Component {
     //grabs all categories from API
     loadCategories = () => {
         API.getCategories().then(res => {
-            console.log("GET call made")
             this.setState({ categories: res.data })
             console.log(this.state.categories)
         }
@@ -51,55 +48,28 @@ class Sidebar extends Component {
 
     //looks for enter
 	handleKeypress(event) {
-        // console.log('handleKeypress:', event.key)
 		if (event.key === "Enter") {
             var category = {
                 UserUuid: this.state.userId,
                 name: this.state.content_add
             }
-
-            var cheat = {
-                id: 0,
-                name:this.state.content_add,
-                updatedAt: "yesterday",
-                UserUuid: this.state.userId
-            }
-            
             API.saveCategory(category);
-            
             this.setState({
                 content_add: ""
-                // categories: this.state.categories.push(cheat)
             });
-            console.log(this.state.categories)
            var time = setTimeout(() => {
             this.loadCategories();
            }, 100); 
         }
-        
     }
-    // componentWillMount() {
-    //     this.loadCategories();
-    // };
 
-    
-    
     //when clicking away from input box, the input box then displays the value of the box
     handleBlur(event) {
         console.log('handleBlur:', event)
 		this.setState({ content_add: "add +" });
     }
-    
-	handleClick(event) {
-        console.log('handleClick:', event)
-		const idToRemove = Number(event.target.dataset["item"]);
-		const newArray = this.state.categories.filter((listitem) => {return listitem.id !== idToRemove});
-		this.setState({ categories: newArray });
-	}
 
     render() {
-        // console.log("checking")
-        // console.log(this.state.categories)
         return (
             <div className="sidenav">
                 {this.state.categories.map(category =>
