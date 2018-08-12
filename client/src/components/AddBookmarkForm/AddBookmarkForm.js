9229fcf02d93c8bff37157e7d3658801d18a1a1d
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-08-11 17:19:47 -0700

    add hover affect

--- client/src/components/AddBookmarkForm/AddBookmarkForm.js
+++ client/src/components/AddBookmarkForm/AddBookmarkForm.js
@@ -21,7 +21,7 @@ class AddBookmarkForm extends React.Component {
     }
 
     handleOpenModal() {
-        this.setState({ showModal: true }); 
+        this.setState({ showModal: true });
     }
 
     handleCloseModal() {
@@ -31,27 +31,27 @@ class AddBookmarkForm extends React.Component {
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
-          [name]: value
+            [name]: value
         });
     };
 
     handleFormSubmit = event => {
         event.preventDefault();
         if (this.state.bookmarkName && this.state.url) {
-          API.saveBookmark({
-            bookmarkName: this.state.bookmarkName,
-            url: this.state.url,
-            description: this.state.description,
-            UserUuid: this.props.UserUuid,
-            CategoryId: this.props.currCategory
-            // SubcategoryId: 1
-          })
+            API.saveBookmark({
+                bookmarkName: this.state.bookmarkName,
+                url: this.state.url,
+                description: this.state.description,
+                UserUuid: this.props.UserUuid,
+                CategoryId: this.props.currCategory
+                // SubcategoryId: 1
+            })
             // .catch(err => console.log(err))
         }
         setTimeout(() => {
             this.props.loadCards;
         }, 100);
-      };
+    };
 
     render() {
         return (
@@ -63,41 +63,41 @@ class AddBookmarkForm extends React.Component {
                     isOpen={this.state.showModal}
                     contentLabel="Minimal Modal Example"
                 >
-                <h3>Add new bookmark</h3>
+                    <h3>Add new bookmark</h3>
                     <form>
                         <div className="form-group">
-                            <input 
-                            className="form-control"
-                            name="bookmarkName" 
-                            value={this.state.bookmarkName}
-                            onChange={this.handleInputChange} 
-                            type="text" 
-                            id={"category-name"}  
-                            placeholder="Bookmark Name" />
+                            <input
+                                className="form-control"
+                                name="bookmarkName"
+                                value={this.state.bookmarkName}
+                                onChange={this.handleInputChange}
+                                type="text"
+                                id={"category-name"}
+                                placeholder="Bookmark Name" />
                         </div>
                         <div className="form-group">
                             <input className="form-control"
-                            name="url"
-                            value={this.state.url}
-                            onChange={this.handleInputChange} 
-                            type="url"  
-                            id="category-url" 
-                            placeholder="Bookmark URL" />
+                                name="url"
+                                value={this.state.url}
+                                onChange={this.handleInputChange}
+                                type="url"
+                                id="category-url"
+                                placeholder="Bookmark URL" />
                         </div>
                         <div className="form-group">
-                            <textarea 
-                            className="form-control" 
-                            name="description"
-                            value={this.state.description}
-                            onChange={this.handleInputChange}
-                            id="category-description" 
-                            rows="3" 
-                            placeholder="Enter a brief description about the bookmark"></textarea>
+                            <textarea
+                                className="form-control"
+                                name="description"
+                                value={this.state.description}
+                                onChange={this.handleInputChange}
+                                id="category-description"
+                                rows="3"
+                                placeholder="Enter a brief description about the bookmark"></textarea>
                         </div>
-                        <button 
-                        type="submit" 
-                        className="btn btn-primary"
-                        onClick={this.handleFormSubmit}>Submit</button>
+                        <button
+                            type="submit"
+                            className="btn btn-primary"
+                            onClick={this.handleFormSubmit}>Submit</button>
                     </form>
                     <button onClick={this.handleCloseModal}>Close Modal</button>
                 </ReactModal>
